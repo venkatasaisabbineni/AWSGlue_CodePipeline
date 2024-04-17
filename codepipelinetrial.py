2e6b7143207eb84e4026df7eefc5b03d31f997de
VenkataSaiSabbineni <venkatasaisabbineni@gmail.com>
2024-04-17 13:27:03 -0400

    added code to create unique card-customer base

--- codepipelinetrial.py
+++ codepipelinetrial.py
@@ -16,25 +16,26 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Read data directly from S3
-source_path = "s3://bobbycodepipeline/input/customer_data.csv"
+source_path = "s3://bobbycodepipeline/scripts/customer_data.csv"
 input_df = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
     format="csv",
     connection_options={"paths": [source_path]},
     format_options={"withHeader": True}
 )
-#Remove null values from the data
+#Remove null values from the data and seperate it based on unique card types.
 df = input_df.toDF()
 df = df.withColumn("Credit_Card_Number", df["Credit_Card_Number"].cast("double"))
-transformed_df = DynamicFrame.fromDF(df.dropna(),glueContext,"transformed_df")
-
-# Convert to csv format
-csv_path = "s3://bobbycodepipeline/output/transformed_customer_data.csv"
-glueContext.write_dynamic_frame.from_options(
-    frame=transformed_df,
-    connection_type="s3",
-    connection_options={"path": csv_path},
-    format="csv"
-)
-
+df_removedna = df.dropna()
+unique_credit_card = df_removedna.select("Credit_Card_Type").distinct().rdd.map(lambda x: x[0]).collect()
+for i in unique_credit_card:
+    df_temp = df_removedna.filter(df_removedna["Credit_Card_Type"] == i)
+    transformed_df = DynamicFrame.fromDF(df_temp, glueContext, f"dynamic_frame_{i}")
+    csv_path = f"s3://bobbycodepipeline/output/{i}_customer_data.csv"
+    glueContext.write_dynamic_frame.from_options(
+        frame=transformed_df,
+        connection_type="s3",
+        connection_options={"path": csv_path},
+        format="csv"
+    )
 job.commit()
